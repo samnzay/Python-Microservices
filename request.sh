c6d7fcfd785b8478a9b7aa16d7d3f334696dd0f3
Samuel Nzayisenga <samnzay@gmail.com>
2023-03-30 18:27:07 +0200

    Update request.sh
    resquest.sh update

--- request.sh
+++ request.sh
@@ -5,7 +5,7 @@ mysql -h 127.0.0.1 -P 3307 -u root -psecret
 curl -X POST http://mp3converter.com/login -u samnzay@gmail.com:Admin123
 
 #Received Token:
-eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InNhbW56YXlAZ21haWwuY29tIiwiZXhwIjoxNjgwMjUzNjAyLCJpYXQiOjE2ODAxNjcyMDIsImFkbWluIjp0cnVlfQ.S82kkCboZnzWFdUjuxasEhoXEDTQ_prtHU8AIrrh9yk
+Token
 
 #Upload Video:
-curl -X POST -F 'file=@./song.mp4' -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InNhbW56YXlAZ21haWwuY29tIiwiZXhwIjoxNjgwMjUzNjAyLCJpYXQiOjE2ODAxNjcyMDIsImFkbWluIjp0cnVlfQ.S82kkCboZnzWFdUjuxasEhoXEDTQ_prtHU8AIrrh9yk' http://mp3converter.com/upload
+curl -X POST -F 'file=@./song.mp4' -H 'Authorization: Bearer {Token}' http://mp3converter.com/upload
