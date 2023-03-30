bb360357e3eab5ad2dc0c9f22a340e9eee050d6c
samnzay <samnzay@gmail.com>
2023-03-30 17:52:25 +0200

    Email Notification Service Created

---
+++ src/notification/consummer.py
@@ -0,0 +1,39 @@
+import pika, sys, os, time
+from send import email
+
+def main():
+
+    #RABBITMQ CONNECTION
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host="rabbitmq")
+        )
+    channel = connection.channel()
+
+    def callback(ch, method, properties, body):
+        err = email.notification(body)
+
+        if err:
+            ch.basic_nack(delivery_tag=method.delivery_tag)
+        else:
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    channel.basic_consume(
+        queue= os.environ.get("MP3_QUEUE"), on_message_callback=callback
+    )
+
+
+    print("Waititing for messages. Press CTRL+C to Exit")
+    channel.start_consuming()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print(" Message Consummer Interrupted")
+
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+
+
