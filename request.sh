6192a0b1159a39b9130e061bea03a1f3e9041bce
Samuel Nzayisenga <samnzay@gmail.com>
2023-03-30 18:28:04 +0200

    Update request.sh
    request.sh file update

--- request.sh
+++ request.sh
@@ -2,7 +2,7 @@
 mysql -h 127.0.0.1 -P 3307 -u root -psecret
 
 #Get GetJWT_Token: 
-curl -X POST http://mp3converter.com/login -u samnzay@gmail.com:Admin123
+curl -X POST http://mp3converter.com/login -u your@email.com:Admin123
 
 #Received Token:
 Token
